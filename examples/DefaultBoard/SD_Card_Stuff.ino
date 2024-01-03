e769514b1eef70a40a2f0905a34487e46b1c7f5b
roflecopter <121650916+roflecopter@users.noreply.github.com>
2024-01-03 11:02:49 +0700

    return block size after sd file created

--- examples/DefaultBoard/SD_Card_Stuff.ino
+++ examples/DefaultBoard/SD_Card_Stuff.ino
@@ -249,7 +249,7 @@ boolean setupSDcard(char limit){
   if(fileIsOpen == true){  // send corresponding file name to controlling program
     if(!board.streaming) {
       Serial0.print("Blocks ");
-      Serial0.print(BLOCK_COUNT);
+      Serial0.println(BLOCK_COUNT);
       Serial0.print("Corresponding SD file ");
       Serial0.println(currentFileName);
       LED_SD_Status_Indication(OK_BLINKS, 250, OK_LED);
